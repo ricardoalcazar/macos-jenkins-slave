1751ee2290c25dc3d43bc244ae463e954deef8f2
Ricardo Alcazar <ricardo.d.alcazar@gmail.com>
2018-09-07 13:25:25 -0500

    Added commands to configure Jenkins user.

--- InstallSlave.py
+++ InstallSlave.py
@@ -1,5 +1,9 @@
-
-
+##
+# This script will install software dependencies onto a base image needed by GMA.
+# It must be run under an account with administrator permissions.
+# Modified: 9/7/2018
+# Author: Ricardo Alcazar
+##
 import CommandManager
 
 # get the sudo password, cast to string
@@ -8,6 +12,10 @@ print(pw)
 
 # get shell manager
 mgr = CommandManager.CommandManager()
+
+# create jenkins user, add to the staff group
+mgr.executeWithSudo("sudo systemadminctl -addUser jenkins -password gmadevop -home /Users/jenkins -uid 1000")
+mgr.executeWithSudo("sudo dseditgroup -o edit -a jenkins -t user staff")
 
 #
 mgr.executeWithSudo("sudo systemsetup -getremotelogin", pw)
